67e87d569b37432f303fa7ad94d18965d190cca9
kelly r maass <kmaass77627@gmail.com>
2019-12-02 02:23:39 +0000

    cli drafting

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,15 +4,32 @@ require_relative "../lib/songmaster.rb"
 class Songmaster::CLI
   
   def greeting
-    puts "Spotifys top 50 songs" 
     list_songs 
+    choose  
+    goodbye
   end 
   
   def list_songs 
     puts "songs "
+    puts "Spotifys top 50 songs" 
   end 
   
-  def 
+  def choose
+    input = nil 
+    while input != exit 
+    "choose a song you would like to know more about?"
+    input = gets.strip
+    case input 
+    when 1 
+      puts more info on song 1
+      when 2 
+        puts more info on song 2
+        end 
+        
+        def goodbye 
+         puts "Thanks for checking out the top 50 spotify songs.
+          see you next time!"
+        end 
 
 
 end 
